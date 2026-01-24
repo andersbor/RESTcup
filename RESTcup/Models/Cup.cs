79fed0d621fac8c636e7a679a23ad36b075b66d0
andersbor <anbo@zealand.dk>
2026-01-24 15:15:03 +0100

    Add project files.

---
+++ RESTcup/Models/Cup.cs
@@ -0,0 +1,26 @@
+﻿namespace RESTcup.Models
+{
+    public class Cup
+    {
+        public int Id { get; set; }
+        public string? Color { get; set; }
+        public int Volume { get; set; }
+
+        public override string ToString()
+        {
+            return $"Cup(Id={Id}, Color={Color}, Volume={Volume})";
+        }
+
+        override public bool Equals(object? obj)
+        {
+            if (obj is not Cup other)
+                return false;
+            return Id == other.Id;
+        }
+
+        override public int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ RESTcup/Models/CupsRepository.cs
@@ -0,0 +1,53 @@
+﻿namespace RESTcup.Models
+{
+    public class CupsRepository
+    {
+        private int nextId = 1;
+        private readonly List<Cup> cups = new();
+
+        public CupsRepository(bool includeData = false)
+        {
+            if (includeData)
+            {
+                Add(new Cup { Color = "Red", Volume = 250 });
+                Add(new Cup { Color = "Blue", Volume = 300 });
+                Add(new Cup { Color = "Green", Volume = 200 });
+            }
+        }
+
+        public IEnumerable<Cup> GetAll()
+        {
+            return new List<Cup>(cups);
+        }
+
+        public Cup Add(Cup cup)
+        {
+            cup.Id = nextId++;
+            cups.Add(cup);
+            return cup;
+        }
+
+        public Cup? GetById(int id)
+        {
+            return cups.FirstOrDefault(cup => cup.Id == id);
+        }
+
+        public bool Remove(int id)
+        {
+            Cup? cup = GetById(id);
+            if (cup is null)
+                return false;
+            return cups.Remove(cup);
+        }
+
+        public bool Update(Cup updatedCup)
+        {
+            Cup? existingCup = GetById(updatedCup.Id);
+            if (existingCup is null)
+                return false;
+            existingCup.Color = updatedCup.Color;
+            existingCup.Volume = updatedCup.Volume;
+            return true;
+        }
+    }
+}
